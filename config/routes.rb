6c3442d6dc6255787f9e0a0e04cd8a453828ae2c
Lucha <ljubomir.lukic73@gmail.com>
2021-04-18 19:11:44 +0200

    Adding links to pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+
   root 'pages#home'
+
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  # This is longer version for learning purposes
+
+  resources :articles
+  # This is shorter version and give as complete REST
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
